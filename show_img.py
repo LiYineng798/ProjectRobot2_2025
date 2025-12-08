c3d4bb8f9a8ce5bc309d911af3b12abf914d2bdc
LiYineng798 <1779271568@qq.com>
2025-12-08 15:02:58 +0800

    Create show_img.py
    This code makes the LED screen cycle through displaying related graphics every three seconds, such as a heart, a happy face, a smile, and so on.

---
+++ show_img.py
@@ -0,0 +1,15 @@
+from microbit import *
+import random
+
+
+images = [
+    Image.HEART,
+    Image.HAPPY,
+    Image.SMILE,
+    Image.SAD
+]
+
+while True:
+    for img in images:
+        display.show(img)
+        sleep(3000)  # 随机 2~3 秒
